77f3ce5b2165e7d762cbab7a9d07af45df68aa20
caina <cainaribeiro.it@outlook.com>
2025-03-29 11:20:16 -0300

    refactor

--- ExpenseSolution/ExpenseSolution.Interfaces/IJwt.cs
+++
@@ -1,9 +0,0 @@
-﻿using ExpenseSolution.DTOs.Users;
-namespace ExpenseSolution.Interfaces
-{
-    public interface IJwt
-    {
-        string GenerateUserToken(UserTokenDTO usertToken);
-        IDictionary<string, object> DecodeToken(string token);
-    }
-}
